03f5986904cb2a62a2e77515e09612666ef9a001
Daniël Heres <danielhers@godatadriven.com>
2020-01-09 09:57:54 +0100

    Use custom relation config

---
+++ dbt/adapters/athena/relation.py
@@ -0,0 +1,47 @@
+from dbt.adapters.base.relation import BaseRelation
+
+
+class AthenaRelation(BaseRelation):
+    DEFAULTS = {
+        'metadata': {
+            'type': 'AthenaRelation'
+        },
+        'quote_character': '"',
+        'quote_policy': {
+            'database': True,
+            'schema': True,
+            'identifier': True,
+        },
+        'include_policy': {
+            'database': False,
+            'schema': True,
+            'identifier': True,
+        },
+        'dbt_created': False,
+
+    }
+
+    SCHEMA = {
+        'type': 'object',
+        'properties': {
+            'metadata': {
+                'type': 'object',
+                'properties': {
+                    'type': {
+                        'type': 'string',
+                        'const': 'AthenaRelation',
+                    },
+                },
+            },
+            'type': {
+                'enum': BaseRelation.RelationTypes + [None]
+            },
+            'path': BaseRelation.PATH_SCHEMA,
+            'include_policy': BaseRelation.POLICY_SCHEMA,
+            'quote_policy': BaseRelation.POLICY_SCHEMA,
+            'quote_character': {'type': 'string'},
+            'dbt_created': {'type': 'boolean'},
+        },
+        'required': ['metadata', 'type', 'path', 'include_policy',
+                     'quote_policy', 'quote_character', 'dbt_created']
+    }
