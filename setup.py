b66d003e8ea295e5a9388f63e6f260d34f76a9be
Denis Zhuk <denis.zhuk@zalando.de>
2020-01-20 17:43:03 +0100

    Update dbt version to 0.15.1

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open(os.path.join(this_directory, 'README.md')) as f:
 
 
 package_name = "dbt-athena"
-package_version = "0.14.0"
+package_version = "0.15.1"
 description = """The athena adpter plugin for dbt (data build tool)"""
 
 setup(
