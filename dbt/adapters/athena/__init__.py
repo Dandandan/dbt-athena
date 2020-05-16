fb0777b2e425fb8ca4b100c9c3d713bda527197f
Heres, Daniel <daniel.heres@kpn.com>
2020-05-16 16:03:30 +0200

    Cleanup, formatting

--- dbt/adapters/athena/__init__.py
+++ dbt/adapters/athena/__init__.py
@@ -1,7 +1,5 @@
-from dbt.adapters.athena.connections import AthenaConnectionManager
 from dbt.adapters.athena.connections import AthenaCredentials
 from dbt.adapters.athena.impl import AthenaAdapter
-from dbt.adapters.athena.relation import AthenaRelation
 
 from dbt.adapters.base import AdapterPlugin
 from dbt.include import athena
@@ -9,4 +7,5 @@ from dbt.include import athena
 Plugin = AdapterPlugin(
     adapter=AthenaAdapter,
     credentials=AthenaCredentials,
-    include_path=athena.PACKAGE_PATH)
+    include_path=athena.PACKAGE_PATH,
+)
